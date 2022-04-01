1c37dfb086d1f6713b61a0a04d8b5e3de8372446
Amrit Singh Sohi <1amritsinghsohi@gmail.com>
2022-04-01 12:29:49 -0400

    setup AdminComponents folder and Admin_Home component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 //import { useEffect, useState } from 'react';
-import { Route, Routes  } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 //import Axios from 'axios';
 import Login from './Component/login/login';
 import Crud from './Component/crud_practice/Crud';
 import 'bootstrap/dist/css/bootstrap.css';
 import Layout from './Component/Layout'
+import Admin_Home from './Component/Admin_Components/admin_home';
 
 
 function App() {
 
   return (
     <Layout>
-          <Routes>
-            
-              <Route exact path="/" element={<Crud />} />
-              <Route exact path="/login" element={<Login />} />
-              <Route path="*" component={() => "404 NOT FOUND"} />
-          </Routes>
-          </Layout>
+      <Routes>
+
+        <Route exact path="/" element={<Crud />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/admin_home" element={Admin_Home} />
+        <Route path="*" component={() => "404 NOT FOUND"} />
+      </Routes>
+    </Layout>
   );
 }
 
